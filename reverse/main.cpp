5bc4302bf340e7bfae727b09ed07be064e3db56b
shanyux <syx1558@163.com>
2017-03-31 14:38:38 +0800

    逆序输出字符串

---
+++ reverse/main.cpp
@@ -0,0 +1,29 @@
+//ÄæĞòÊä³ö×Ö·û´®
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+void reverse(char ch[], int left, int right)
+{
+    while(left < right)
+    {
+        char c = ch[left];
+        ch[left++] = ch[right];
+        ch[right--] = c;
+
+    }
+
+
+}
+int main()
+{
+    char ch[100];
+    cout << "ÇëÊäÈë×Ö·û£º" << endl;
+    cin >> ch;
+    int length = strlen(ch);
+   // cout << length << sizeof(ch) << endl;;
+    reverse(ch, 0, length-1);
+    for(int i=0; i<length; i++)
+        cout<<ch[i];
+    return 0;
+}
